3cc7e87ac8f7a6112397419c4a23663c5ba2dcef
Jordan Penard <jordan.penard@thinci.com>
2019-11-29 15:35:55 +0000

    First implementation of a rough login with flask login

--- website/python/brewmonitor/views.py
+++ website/python/brewmonitor/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, request, redirect
 from flask_mako import render_template
+
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user 
+from brewmonitor.user import User
 
 home_bp = Blueprint(
     'home',
@@ -14,9 +17,27 @@ def index():
         'home.html.mako'
     )
 
-
-@home_bp.route('/login')
+@home_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    # TODO(tr) implement using flask-login
-    # TODO(tr) Handle the next parameter so we can redirect to the appropriate page.
-    return ''
+        
+    if request.method == 'GET':
+        return render_template('login.html.mako')
+
+    else:
+        username = request.form.get('username')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+        
+        id = User.verify(username, password)
+        if id != 0:
+            login_user(User(id), remember=remember)
+            return redirect(url_for('home.index'))
+
+        # if the above check passes, then we know the user has the right credentials
+        return redirect(url_for('home.login')) # if user doesn't exist or password is wrong, reload the page
+
+@home_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home.index'))
